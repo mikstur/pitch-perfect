216397e1b0c90d7d00363a89da2a5e159baea70d
Miki von Ketelhodt <vonketelhodtm@gmail.com>
2015-05-09 14:59:59 +0200

    Initial Commit

---
+++ Pitch Perfect/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pitch Perfect
+//
+//  Created by Miki von Ketelhodt on 2015/05/09.
+//  Copyright (c) 2015 RBG Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
